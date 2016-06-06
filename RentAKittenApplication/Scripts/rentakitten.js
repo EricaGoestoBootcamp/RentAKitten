6bfa33b8a186029b6213312c123ad3ad654e1eb0
Erica Sommer <erica.sommer@att.net>
2016-06-05 22:19:49 -0400

    working carousel, populated. no nav yet.

--- RentAKittenApplication/Scripts/rentakitten.js
+++ RentAKittenApplication/Scripts/rentakitten.js
@@ -1,8 +1,20 @@
-﻿var carousel;
-$(document).ready(function () {
-    carousel = $("ul");
-    carousel.itemslide(
-        {
-            one_item: true
-        })
+﻿$(document).ready(function () {
+
+    //test
+    console.log("Hi");
+
+    // Activate Carousel
+    $("#myCarousel").carousel({ interval: false, wrap: true });
+
+    // Enable Carousel Indicators
+    //$(".item").click(function () {
+    //    $("#myCarousel").carousel(1);
+   // });
+
+    // Enable Carousel Controls
+    $(".left").click(function () {
+        $("#myCarousel").carousel("prev");
+    });
+
+ 
 });
